f86b29450733b674af0e66a54b14403e037c536a
Tinaguhwa23 <tinaguhwa@gmail.com>
2024-05-19 19:20:30 +0000

    deploy static commit

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ distributes an archive to the web servers
 from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists, isdir
-env.hosts = ['142.44.167.228', '144.217.246.195']
+env.hosts = ['54.162.51.145', '52.91.120.73']
 
 
 def do_pack():
